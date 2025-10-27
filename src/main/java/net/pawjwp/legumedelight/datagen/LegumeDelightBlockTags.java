902998e70e710a47de202425d0f344d6f45d44b8
Jeremy <pawjwp@users.noreply.github.com>
2025-10-27 13:02:37 -0400

    Added datagen for tagged items/blocks

--- src/main/java/net/pawjwp/legumedelight/datagen/LegumeDelightBlockTags.java
+++ src/main/java/net/pawjwp/legumedelight/datagen/LegumeDelightBlockTags.java
@@ -22,5 +22,51 @@ public class LegumeDelightBlockTags extends BlockTagsProvider {
     @Override
     protected void addTags(HolderLookup.Provider pProvider) {
 
+        // Mineability Tags
+        this.tag(net.minecraft.tags.BlockTags.MINEABLE_WITH_HOE).add(
+                LegumeDelightBlocks.BEAN_SACK.get(),
+                LegumeDelightBlocks.PEANUT_SACK.get()
+        );
+
+        // Minecraft Tags
+        this.tag(net.minecraft.tags.BlockTags.CLIMBABLE).add(
+                LegumeDelightBlocks.BEAN_CROP.get()
+        );
+        this.tag(net.minecraft.tags.BlockTags.CROPS).add(
+                LegumeDelightBlocks.BUDDING_BEAN_CROP.get(),
+                LegumeDelightBlocks.BEAN_CROP.get(),
+                LegumeDelightBlocks.PEANUT_CROP.get()
+        );
+        this.tag(net.minecraft.tags.BlockTags.SMALL_FLOWERS).add(
+                LegumeDelightBlocks.WILD_BEANS.get(),
+                LegumeDelightBlocks.WILD_PEANUTS.get()
+        );
+        this.tag(net.minecraft.tags.BlockTags.MAINTAINS_FARMLAND).add(
+                LegumeDelightBlocks.BUDDING_BEAN_CROP.get(),
+                LegumeDelightBlocks.BEAN_CROP.get(),
+                LegumeDelightBlocks.PEANUT_CROP.get()
+        );
+
+        // Forge Tags
+        this.tag(LegumeDelightTags.STORAGE_BLOCKS_BLOCK_BEAN).add(LegumeDelightBlocks.BEAN_SACK.get());
+        this.tag(LegumeDelightTags.STORAGE_BLOCKS_BLOCK_PEANUT).add(LegumeDelightBlocks.PEANUT_SACK.get());
+
+        // Farmer's Delight Tags
+        this.tag(ModTags.WILD_CROPS).add(
+                LegumeDelightBlocks.WILD_BEANS.get(),
+                LegumeDelightBlocks.WILD_PEANUTS.get()
+        );
+
+        // Compatibility Tags
+        this.tag(CompatibilityTags.SERENE_SEASONS_SPRING_CROPS_BLOCK).add(
+                LegumeDelightBlocks.BUDDING_BEAN_CROP.get(),
+                LegumeDelightBlocks.BEAN_CROP.get(),
+                LegumeDelightBlocks.PEANUT_CROP.get()
+        );
+        this.tag(CompatibilityTags.SERENE_SEASONS_SUMMER_CROPS_BLOCK).add(
+                LegumeDelightBlocks.BUDDING_BEAN_CROP.get(),
+                LegumeDelightBlocks.BEAN_CROP.get(),
+                LegumeDelightBlocks.PEANUT_CROP.get()
+        );
     }
 }
--- src/main/java/net/pawjwp/legumedelight/datagen/LegumeDelightItemTags.java
+++ src/main/java/net/pawjwp/legumedelight/datagen/LegumeDelightItemTags.java
@@ -26,5 +26,111 @@ public class LegumeDelightItemTags extends ItemTagsProvider {
     @Override
     protected void addTags(HolderLookup.Provider pProvider) {
 
+        // Minecraft Tags
+        this.tag(ItemTags.VILLAGER_PLANTABLE_SEEDS).add(
+                LegumeDelightItems.BEANS.get(),
+                LegumeDelightItems.PEANUTS.get()
+        );
+
+        // Forge Tags
+        this.tag(ForgeTags.CROPS).add(
+                LegumeDelightItems.BEANS.get(),
+                LegumeDelightItems.PEANUTS.get()
+        );
+        this.tag(LegumeDelightTags.CROPS_BEAN).add(LegumeDelightItems.BEANS.get());
+        this.tag(LegumeDelightTags.CROPS_PEANUT).add(LegumeDelightItems.PEANUTS.get());
+
+        this.tag(LegumeDelightTags.LEGUMES).add(
+                LegumeDelightItems.BEANS.get(),
+                LegumeDelightItems.PEANUTS.get()
+        );
+        this.tag(LegumeDelightTags.LEGUMES_BEAN).add(LegumeDelightItems.BEANS.get());
+        this.tag(LegumeDelightTags.LEGUMES_PEANUT).add(LegumeDelightItems.PEANUTS.get());
+
+
+        this.tag(LegumeDelightTags.BEAN).add(LegumeDelightItems.BEANS.get());
+        this.tag(LegumeDelightTags.BEANS).add(LegumeDelightItems.BEANS.get());
+        this.tag(LegumeDelightTags.BEANS_BEAN).add(LegumeDelightItems.BEANS.get());
+
+        this.tag(LegumeDelightTags.BEAN_PASTE).add(LegumeDelightItems.BEAN_PASTE.get());
+        this.tag(LegumeDelightTags.RED_BEAN_PASTE).add(LegumeDelightItems.BEAN_PASTE.get());
+        this.tag(LegumeDelightTags.REFRIED_BEANS).add(LegumeDelightItems.REFRIED_BEANS.get());
+
+
+        this.tag(LegumeDelightTags.PEANUT).add(LegumeDelightItems.PEANUTS.get());
+        this.tag(LegumeDelightTags.PEANUTS).add(LegumeDelightItems.PEANUTS.get());
+        this.tag(LegumeDelightTags.NUTS).add(LegumeDelightItems.PEANUTS.get());
+        this.tag(LegumeDelightTags.NUTS_PEANUT).add(LegumeDelightItems.PEANUTS.get());
+
+        this.tag(LegumeDelightTags.NUT_BUTTER).add(LegumeDelightItems.PEANUT_BUTTER.get());
+        this.tag(LegumeDelightTags.PEANUT_BUTTER).add(LegumeDelightItems.PEANUT_BUTTER.get());
+
+        this.tag(ForgeTags.BERRIES); // establish tag exists before use
+        this.tag(LegumeDelightTags.FRUITS).add(
+                Items.APPLE,
+                Items.MELON_SLICE,
+                Items.CHORUS_FRUIT
+        ).addTag(
+                ForgeTags.BERRIES
+        );
+
+        this.tag(LegumeDelightTags.STORAGE_BLOCKS_ITEM_BEAN).add(LegumeDelightItems.BEAN_SACK.get());
+        this.tag(LegumeDelightTags.STORAGE_BLOCKS_ITEM_PEANUT).add(LegumeDelightItems.PEANUT_SACK.get());
+
+        // Farmer's Delight Tags
+        this.tag(ModTags.MEALS).add(
+                LegumeDelightItems.BAKED_BEANS.get(),
+                LegumeDelightItems.BEAN_BURGER.get(),
+                LegumeDelightItems.BEAN_BURRITO.get(),
+                LegumeDelightItems.BEANS_AND_RICE.get(),
+                LegumeDelightItems.BEANS_ON_TOAST.get(),
+                LegumeDelightItems.CHILI.get(),
+                LegumeDelightItems.NACHOS.get(),
+                LegumeDelightItems.PAD_THAI.get(),
+                LegumeDelightItems.PASTA_WITH_TOMATO_SAUCE.get(),
+                LegumeDelightItems.PEANUT_BUTTER_AND_JAM_SANDWICH.get(),
+                LegumeDelightItems.POTATO_TACO.get()
+        );
+
+        // Compatibility Tags
+        this.tag(CompatibilityTags.CREATE_UPRIGHT_ON_BELT).add(
+                LegumeDelightItems.BEAN_PASTE.get(),
+                LegumeDelightItems.PEANUT_BUTTER.get(),
+                LegumeDelightItems.REFRIED_BEANS.get(),
+                LegumeDelightItems.MOONCAKE.get()
+        );
+        this.tag(CompatibilityTags.SERENE_SEASONS_SPRING_CROPS).add(
+                LegumeDelightItems.BEANS.get(),
+                LegumeDelightItems.PEANUTS.get()
+        );
+        this.tag(CompatibilityTags.SERENE_SEASONS_SUMMER_CROPS).add(
+                LegumeDelightItems.BEANS.get(),
+                LegumeDelightItems.PEANUTS.get()
+        );
+        this.tag(CompatibilityTags.TINKERS_CONSTRUCT_SEEDS).add(
+                LegumeDelightItems.BEANS.get(),
+                LegumeDelightItems.PEANUTS.get()
+        );
+
+
+
+        this.tag(LegumeDelightTags.DIET_PROTEINS).add(
+                LegumeDelightItems.BEANS.get(),
+                LegumeDelightItems.PEANUTS.get()
+        ).addOptional(ResourceLocation.parse("culturaldelights:eggplant_parmesian"));;
+
+        this.tag(LegumeDelightTags.DIET_GRAINS)
+                .addOptional(ResourceLocation.parse("culturaldelights:corn_cob"))
+                .addOptional(ResourceLocation.parse("culturaldelights:corn_kernels"))
+                .addOptional(ResourceLocation.parse("culturaldelights:eggplant_parmesian"));
+
+        this.tag(LegumeDelightTags.DIET_VEGETABLES)
+                .addOptional(ResourceLocation.parse("culturaldelights:eggplant_parmesian"));
+
+        // This doesn't seem to take effect even though the data generates right. Maybe due to load orders? I'll leave it in just in case.
+        this.tag(LegumeDelightTags.DIET_VEGETABLES).remove(
+                Items.PUMPKIN_SEEDS,
+                Items.SUNFLOWER
+        );
     }
 }
