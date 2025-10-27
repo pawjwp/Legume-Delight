43d3f73e7f8a6b0a3bfe7a57fd9b02cb3c4caf43
Jeremy <pawjwp@users.noreply.github.com>
2025-10-27 13:03:04 -0400

    Added datagen for loot tables

--- src/main/java/net/pawjwp/legumedelight/datagen/loot/LegumeDelightBlockLootTables.java
+++ src/main/java/net/pawjwp/legumedelight/datagen/loot/LegumeDelightBlockLootTables.java
@@ -27,7 +27,54 @@ public class LegumeDelightBlockLootTables extends BlockLootSubProvider {
 
     @Override
     protected void generate() {
+        this.dropSelf(LegumeDelightBlocks.BEAN_SACK.get());
+        this.dropSelf(LegumeDelightBlocks.PEANUT_SACK.get());
 
+        this.dropOther(LegumeDelightBlocks.BUDDING_BEAN_CROP.get(), LegumeDelightItems.BEANS.get());
+
+        LootItemCondition.Builder beansLootable = LootItemBlockStatePropertyCondition.hasBlockStateProperties(LegumeDelightBlocks.BEAN_CROP.get())
+                .setProperties(StatePropertiesPredicate.Builder.properties().hasProperty(PeanutBlock.AGE, 7));
+        this.add(LegumeDelightBlocks.BEAN_CROP.get(), this.applyExplosionDecay(LegumeDelightBlocks.BEAN_CROP.get(),
+                LootTable.lootTable()
+                        .withPool(LootPool.lootPool()
+                                .add(LootItem.lootTableItem(LegumeDelightItems.BEANS.get()))
+                        )
+                        .withPool(LootPool.lootPool()
+                                .when(beansLootable)
+                                .add(LootItem.lootTableItem(LegumeDelightItems.BEANS.get())
+                                        .apply(ApplyBonusCount.addBonusBinomialDistributionCount(Enchantments.BLOCK_FORTUNE, 0.5714286F, 3))
+                                )
+                        )
+        ));
+
+        LootItemCondition.Builder peanutsLootable = LootItemBlockStatePropertyCondition.hasBlockStateProperties(LegumeDelightBlocks.PEANUT_CROP.get())
+                .setProperties(StatePropertiesPredicate.Builder.properties().hasProperty(PeanutBlock.AGE, 7));
+        this.add(LegumeDelightBlocks.PEANUT_CROP.get(), this.applyExplosionDecay(LegumeDelightBlocks.PEANUT_CROP.get(),
+                LootTable.lootTable()
+                        .withPool(LootPool.lootPool()
+                                .add(LootItem.lootTableItem(LegumeDelightItems.PEANUTS.get()))
+                        )
+                        .withPool(LootPool.lootPool()
+                                .when(peanutsLootable)
+                                .add(LootItem.lootTableItem(LegumeDelightItems.PEANUTS.get())
+                                        .apply(ApplyBonusCount.addBonusBinomialDistributionCount(Enchantments.BLOCK_FORTUNE, 0.5714286F, 3))
+                                )
+                        )
+        ));
+
+        this.add(LegumeDelightBlocks.WILD_BEANS.get(),
+                createShearsDispatchTable(LegumeDelightBlocks.WILD_BEANS.get(),this.applyExplosionDecay(LegumeDelightBlocks.WILD_BEANS.get(),
+                        LootItem.lootTableItem(LegumeDelightItems.BEANS.get())
+                                .apply(ApplyBonusCount.addUniformBonusCount(Enchantments.BLOCK_FORTUNE, 2))
+                ))
+        );
+
+        this.add(LegumeDelightBlocks.WILD_PEANUTS.get(),
+                createShearsDispatchTable(LegumeDelightBlocks.WILD_PEANUTS.get(),this.applyExplosionDecay(LegumeDelightBlocks.WILD_PEANUTS.get(),
+                        LootItem.lootTableItem(LegumeDelightItems.PEANUTS.get())
+                                .apply(ApplyBonusCount.addUniformBonusCount(Enchantments.BLOCK_FORTUNE, 2))
+                ))
+        );
     }
 
     @Override
