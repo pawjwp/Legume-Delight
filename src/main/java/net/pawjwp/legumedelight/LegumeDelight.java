a15869a0bc576cab729bc575beeee379e1dd8704
Jeremy <pawjwp@users.noreply.github.com>
2026-09-12 12:58:44 -0400

    Removed some unused code from the original MDK

--- src/main/java/net/pawjwp/legumedelight/LegumeDelight.java
+++ src/main/java/net/pawjwp/legumedelight/LegumeDelight.java
@@ -4,17 +4,11 @@ import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
 
-import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
-import net.neoforged.neoforge.event.server.ServerStartingEvent;
 
 import net.pawjwp.legumedelight.block.LegumeDelightBlocks;
 import net.pawjwp.legumedelight.item.LegumeDelightItems;
@@ -37,39 +31,19 @@ public class LegumeDelight {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
-        // Register ourselves for server and other game events we are interested in
-        NeoForge.EVENT_BUS.register(this);
-
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event)
     {
-        // Some common setup code
-        LOGGER.info("HELLO FROM COMMON SETUP");
+        LOGGER.info("Legume Delight loaded");
     }
 
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
         if(event.getTab() == ModCreativeTabs.TAB_FARMERS_DELIGHT.get()) {
             LegumeDelightItems.CREATIVE_TAB_ITEMS.forEach(item -> event.accept(item.get()));
-        }
-    }
-
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event)
-    {
-    }
-
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @EventBusSubscriber(modid = MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents
-    {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event)
-        {
         }
     }
 }
